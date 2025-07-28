08a51e23aac787a484a4b32d2160b5d5a4a478d2
banasu0723 <banasu0723@gmail.com>
2025-07-28 16:18:16 +0900

    add : 예약 관련 enum 추가

---
+++ src/main/java/gc/goormcinema/global/common/enums/NotificationType.java
@@ -0,0 +1,8 @@
+package gc.goormcinema.global.common.enums;
+
+public enum NotificationType {
+    RESERVATION_CONFIRMED,
+    RESERVATION_CANCELLED,
+    RESERVATION_REMINDER,
+    PAYMENT_COMPLETED
+}

1406c9175dc00937650516056a06e23ac481a54b
Hwang-sebin <sbsimon0701@gmail.com>
2025-08-06 02:07:06 +0900

    ✨[Feature] 채팅 도메인 Enum 클래스 구현
      채팅 기능에 필요한 6개 Enum 클래스 구현
    
      구현된 Enum:
      - ChatRoomStatus: 채팅방 상태 (ACTIVE, CLOSED)
      - ChatRoomType: 채팅방 유형 (INQUIRY, GENERAL)
      - MessageStatus: 메시지 상태 (SENT, READ)
      - MessageType: 메시지 타입 (TEXT, FILE, IMAGE)
      - ParticipantRole: 참여자 역할 (CUSTOMER, ADMIN)
      - NotificationType: 알림 타입 (MESSAGE, ROOM_CREATED, etc.)

---
+++ src/main/java/gc/goormcinema/domain/chat/entity/MessageStatus.java
@@ -0,0 +1,6 @@
+package gc.goormcinema.domain.chat.entity;
+
+public enum MessageStatus {
+    SENT,
+    READ
+}
---
+++ src/main/java/gc/goormcinema/domain/chat/entity/NotificationType.java
@@ -0,0 +1,8 @@
+package gc.goormcinema.domain.chat.entity;
+
+public enum NotificationType {
+    NEW_MESSAGE,
+    ROOM_CREATED,
+    ROOM_CLOSED,
+    MENTION
+}

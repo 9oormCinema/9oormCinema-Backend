c4f80fb9cd18a263c91ad5dbe06b4bc3ed2dfba5
juneon <juneon0830@naver.com>
2025-08-06 13:21:41 +0900

    이벤트 상세 페이지, 리뷰 보기 및 추가 생성

--- src/main/java/gc/goormcinema/domain/events/service/EventsService.java
+++ src/main/java/gc/goormcinema/domain/events/service/EventsService.java
@@ -1,7 +1,12 @@
 package gc.goormcinema.domain.events.service;
 
+import gc.goormcinema.domain.events.dto.response.CreateReviewResponse;
+import gc.goormcinema.domain.events.dto.response.EventDetailResponse;
+import gc.goormcinema.domain.events.dto.response.ReviewListResponse;
 import gc.goormcinema.domain.events.dto.response.SearchEventsResponse;
 import gc.goormcinema.domain.events.entity.Event;
+import gc.goormcinema.domain.events.entity.EventDetail;
+import gc.goormcinema.domain.events.entity.Venue;
 import gc.goormcinema.domain.events.enums.Sort;
 import gc.goormcinema.domain.events.repository.EventsRepository;
 import org.springframework.stereotype.Service;
@@ -64,4 +69,29 @@ public class EventsService {
                 ))
                 .collect(Collectors.toList());
     }
+
+    public EventDetailResponse getEventDetail(Long event_id){
+        System.out.println("event-detail => service 동작함");
+        Long venue_id = eventsRepository.findVenueIdByEventId(event_id);
+        Venue venue = eventsRepository.findVenueByVenueId(venue_id);
+        EventDetail eventDetail = eventsRepository.findEventDetailByEventId(event_id);
+
+        return new EventDetailResponse(eventDetail.getEventId(),
+                eventDetail.getTitle(),
+                eventDetail.getPosterUrl(),
+                venue, eventDetail.getStartsAt(),
+                eventDetail.getEndsAt(), eventDetail.getFee(),
+                eventDetail.getAvgRating(), eventDetail.getHits(),
+                eventDetail.getCreatedAt());
+    }
+
+    public CreateReviewResponse createReview(Long event_id, Long user_id, Integer rating, String comment){
+        eventsRepository.updateRating(event_id, rating);
+        return eventsRepository.createReview(event_id, user_id, rating, comment);
+    }
+
+    public List<ReviewListResponse> getReviews(Long eventId){
+        return eventsRepository.getReviews(eventId);
+    }
+
 }
